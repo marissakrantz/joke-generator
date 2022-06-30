44f7da270fab3eeef9155a429043983b2dd06320
Marissa Tobin <marissatobin@192-168-1-100.tpgi.com.au>
2022-06-30 12:40:07 +0800

    add local stroage and fix css/html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 var fetchButton = document.getElementById('jokeGenLink');
 var jokeResult = document.getElementById('joke-item');
 var gifImage = document.getElementById('gif-image');
+const submitEl = document.getElementById('submit');
 const apiKey = '196449f4f1604e23823c2a17bf6ba22b';
 const apiKeyGiphy = 'ZWjAKp5P86jpqRuDFI5NpQkLD4KIFrn0';
 const baseURL = "https://v2.jokeapi.dev";
+const searchJokeInput = document.getElementById('search-joke-input');
+const searchHistory = document.getElementById('search-history');
 
 
 function getApi() {
@@ -56,15 +59,84 @@ function generateGifImage(jokeInput) {
     });
 
 }
+function showSearchJokeResult(event) {
+  //alert("Inside showSearchJokeResult");
+  // Prevent default action
+  event.preventDefault();
+  //console.log(event);
+
+  var searchInput = searchJokeInput.value;
+  console.log(searchInput);
+  var requestUrl = `https://v2.jokeapi.dev/joke/Any?contains=${searchInput}`;
+  console.log("requestUrl" + requestUrl)
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("search Joke " + data);
+      if (data != '') {
+        console.log("Output" + data.joke);
+      }
+      //jokeStr = data.joke;
+      jokeResult.textContent = "";
+
+      var createSearchDiv = document.createElement('div');
+      //createDiv.addClass("blue-text text-darken-2");
+      //createDiv.addClass("divText");
+      createSearchDiv.textContent = data.joke;
+      jokeResult.appendChild(createSearchDiv);
+
+    });
+
+  generateGifImage(searchInput);
+
+  storeSearchJokeKeyword(searchInput);
+
+}
+function storeSearchJokeKeyword(searchJokeKeyword) {
+  let savedSearchKey = localStorage.getItem('searchJokeKeyword');
+  let searchJokeList;
+  if (savedSearchKey === null) {
+    searchJokeList = [];
+  } else {
+    searchJokeList = JSON.parse(searchJokeList);
+  }
+  var userSearchKey = {
+    keyword: searchJokeKeyword
+  };
+  searchJokeList.push(userSearchKey);
+  console.log("highScoresList" + searchJokeList);
+
+  localStorage.setItem('jokeSearch', JSON.stringify(searchJokeList));
+  console.log("calling populateHighScores");
+  showSearchJokeKeyword();
+
+}
+
+function showSearchJokeKeyword() {
+  let storedSearchKeyword = localStorage.getItem('jokeSearch');
+  console.log("inside populateHighScores1" + storedSearchKeyword);
+  if (storedSearchKeyword === null) {
+    return;
+  }
+  var savedKeyword = JSON.parse(storedSearchKeyword);
+  console.log("List Length" + savedKeyword.length);
+  for (let i = 0; i < savedKeyword.length; i++) {
+    var eachSearchDiv = document.createElement("div");
+    eachSearchDiv.textContent = savedKeyword[i].keyword;
+    searchHistory.appendChild(eachSearchDiv);
+    console.log("local storage" + savedKeyword[i].keyword);
+    //list.appendChild(eachSearch);
+  }
+}
 
 fetchButton.addEventListener('click', getApi);
+submitEl.addEventListener('click', showSearchJokeResult);
 
-//window.localStorage.setItem('joke', JSON.stringify(this.id));
-//window.localStorage.getItem('joke', JSON.stringify(this.id));
-
-$("#clearHistory").on("click", function (event) {
+$("#clear").on("click", function (event) {
   event.preventDefault();
-  randomJokeList = [];
-  localStorage.removeItem("randomJokeList");
+  jokeSearch = [];
+  localStorage.removeItem("jokeSearch");
   document.location.reload();
 });
